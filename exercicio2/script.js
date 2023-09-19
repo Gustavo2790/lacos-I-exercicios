aeea5ffc5c3d777885413954b59579ac8c3a294f
Gustavo <gustavosoares2790@gmail.com>
2023-09-19 19:55:49 -0300

    Exercícios de fixação laços I

---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let numeroUsuario = Number(prompt('Insira um número:'))
+
+for(i = 1; i <= 10; i++){
+    let multiplicação = numeroUsuario * i
+    console.log(`O número digitado é: ${numeroUsuario} e a multiplicação dele por: ${i} é igaul a: ${multiplicação}`)
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+
+const rankeiaArray = () => {
+    const array = ["Zenilda", "Paulo", "Gláucia", "Gustavo", "Guilherme"]
+    for(i = 0; i < array.length; i++){
+        console.log(`${i+1} - ${array[i]}`)
+    }
+}
+
+rankeiaArray()
